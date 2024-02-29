5c0bc5ffdb8fd84cc8426e11bea41e223c03af79
tahirbaba <161713622+tahirbaba@users.noreply.github.com>
2024-02-29 18:46:24 +0500

    RESTURANT

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("Tooba'Resturant");
+// string data type
+console.log("Chicken karahi = 499");
+console.log("Beef karahi");
+console.log("Mutton karahi");
+console.log("Chicken Biryani");
+console.log("Beef Biryani");
+console.log("Mutton Biryani");
+//Number data type
---
+++ main.ts
@@ -0,0 +1,9 @@
+console.log("Tooba'Resturant");
+// string data type
+console.log("Chicken karahi = 1499 PKR");
+console.log("Beef karahi = 1699 PKR");
+console.log("Mutton karahi  = 1899 PKR");
+console.log("Chicken Biryani = 499 PKR");
+console.log("Beef Biryani = 599 PKR");
+console.log("Mutton Biryani = 699 PKR");
+//Number data type
